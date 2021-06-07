d198db6496d4830faf4cbf0004c77128e6db01b9
BecaLParker <becaparker@gmail.com>
2021-06-07 13:41:11 +0100

    refactor to extract temperature updation to its own function

--- interface.js
+++ interface.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const updateTemperature = () => {
+    document.querySelector('#temperature').innerText = thermostat.temperature;
+  }
   const thermostat = new Thermostat();
-  document.querySelector('#temperature').innerText = thermostat.temperature
+  updateTemperature();
 
   document.querySelector('#temperature-up').addEventListener('click', () => {
     thermostat.up();
-    document.querySelector('#temperature').innerText = thermostat.temperature
+    updateTemperature();
   })
 
   document.querySelector('#temperature-down').addEventListener('click', () => {
     thermostat.down();
-    document.querySelector('#temperature').innerText = thermostat.temperature
+    updateTemperature();
   })
 })
