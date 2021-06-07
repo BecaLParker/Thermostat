f6f6aa44169b149c9eadcd4492c25a5cc5f6e154
BecaLParker <becaparker@gmail.com>
2021-06-07 13:34:09 +0100

    changing temperature with button clicks

--- interface.js
+++ interface.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   const thermostat = new Thermostat();
   document.querySelector('#temperature').innerText = thermostat.temperature
+
+  document.querySelector('#temperature-up').addEventListener('click', () => {
+    thermostat.up();
+    document.querySelector('#temperature').innerText = thermostat.temperature
+  })
+
+  document.querySelector('#temperature-down').addEventListener('click', () => {
+    thermostat.down();
+    document.querySelector('#temperature').innerText = thermostat.temperature
+  })
 })
