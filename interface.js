803fd3475b0381e54dfec295faa4c6acd7cea88c
BecaLParker <becaparker@gmail.com>
2021-06-09 11:14:22 +0100

    SEntering user city for weather

--- interface.js
+++ interface.js
@@ -1,43 +1,52 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=Cambridge,uk&appid=722da2eaa18da16838a06a3f8d9dbe21&units=metric')
-    .then((response) => {
-      return response.json()
-    })
-    .then((data) => {
-      document.querySelector('#current-weather').innerText = Math.floor(data.main.temp);
-    });
+  document.querySelector("#select-city").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const city = document.querySelector("#current-city").value;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},uk&appid=722da2eaa18da16838a06a3f8d9dbe21&units=metric`;
+
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        document.querySelector("#current-weather").innerText = Math.floor(
+          data.main.temp
+        );
+        document.querySelector("#display-current-city").innerText = city
+      });
+  });
 
   const updateTemperature = () => {
-    document.querySelector('#temperature').innerText = thermostat.temperature;
-    document.querySelector('#temperature').className = thermostat.energyUsage();
-  }
+    document.querySelector("#temperature").innerText = thermostat.temperature;
+    document.querySelector("#temperature").className = thermostat.energyUsage();
+  };
   const thermostat = new Thermostat();
   updateTemperature();
 
-  document.querySelector('#temperature-up').addEventListener('click', () => {
+  document.querySelector("#temperature-up").addEventListener("click", () => {
     thermostat.up();
     updateTemperature();
-  })
+  });
 
-  document.querySelector('#temperature-down').addEventListener('click', () => {
+  document.querySelector("#temperature-down").addEventListener("click", () => {
     thermostat.down();
     updateTemperature();
-  })
+  });
 
-  document.querySelector('#temperature-reset').addEventListener('click', () => {
+  document.querySelector("#temperature-reset").addEventListener("click", () => {
     thermostat.resetTemperature();
     updateTemperature();
-  })
+  });
 
-  document.querySelector('#power-saving-on').addEventListener('click', () => {
+  document.querySelector("#power-saving-on").addEventListener("click", () => {
     thermostat.switchPowerSavingModeOn();
-    document.querySelector('#power-saving-status').innerText = 'on';
+    document.querySelector("#power-saving-status").innerText = "on";
     updateTemperature();
-  })
+  });
 
-  document.querySelector('#power-saving-off').addEventListener('click', () => {
+  document.querySelector("#power-saving-off").addEventListener("click", () => {
     thermostat.switchPowerSavingModeOff();
-    document.querySelector('#power-saving-status').innerText = 'off';
+    document.querySelector("#power-saving-status").innerText = "off";
     updateTemperature();
-  })
-})
+  });
+});
