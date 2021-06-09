20c5981fce3a034fb914d4f40d580cb7d8e63933
BecaLParker <becaparker@gmail.com>
2021-06-09 11:20:08 +0100

    refactors fetch to add Cambridge by default

--- README.md
+++ README.md
@@ -26,10 +26,12 @@ Specification:
 * Power saving mode is on by default but it can also be turned off
 * You can reset the temperature to 20 with a reset function
 * You can ask about the thermostat's current energy usage: < 18 is `low-usage`, <= 25 is `medium-usage`, anything else is `high-usage`.
-* (In the challenges where we add an interface, low-usage will be indicated with green, medium-usage indicated with black, high-usage indicated with red.)
+* low-usage will be indicated with green, medium-usage indicated with black, high-usage indicated with red.
+* By default, weather for Cambridge, UK is displayed.
+* You can enter a different UK city to update the weather display.
 
 ## Next steps
-[APIs (weather)](https://github.com/makersacademy/course/blob/master/thermostat_es6/apis.md)
+[Styling](https://github.com/makersacademy/course/blob/master/thermostat_es6/styling.md)
 
 ## Credits
 Pair-programmed the business logic with [Dewald Viljoen](https://github.com/Dev-ops-true#dewald-viljoen-dev). 
--- interface.js
+++ interface.js
@@ -1,19 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const displayWeather = (city) => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},uk&appid=722da2eaa18da16838a06a3f8d9dbe21&units=metric`;
+    fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      document.querySelector("#current-weather").innerText = Math.floor(
+        data.main.temp
+      );
+      document.querySelector("#display-current-city").innerText = city
+    });
+  }
+  displayWeather('Cambridge');
+
   document.querySelector("#select-city").addEventListener("submit", (event) => {
     event.preventDefault();
     const city = document.querySelector("#current-city").value;
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},uk&appid=722da2eaa18da16838a06a3f8d9dbe21&units=metric`;
 
-    fetch(url)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        document.querySelector("#current-weather").innerText = Math.floor(
-          data.main.temp
-        );
-        document.querySelector("#display-current-city").innerText = city
-      });
+    displayWeather(city);
   });
 
   const updateTemperature = () => {
